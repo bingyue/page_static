27fffeb53c1278b941d326c0380b5c2e51624180
danielbing <danielbing@sohu-inc.com>
2016-12-01 19:46:40 +0800

    change server

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-#!/usr/bin/env node
+var http = require('http');
+var fs = require('fs');//引入文件读取模块
 
-var express = require("express"),
-    app = express(),
-    bodyParser = require('body-parser'),
-    errorHandler = require('errorhandler'),
-    methodOverride = require('method-override'),
-    hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 9090,
-    publicDir = process.argv[2] || __dirname + '/view',
-    path = require('path');
+var port = parseInt(process.env.PORT, 10) || 8888;
+var hostname = process.env.HOSTNAME || 'localhost';
+//需要访问的文件的存放目录
+var publicDir = process.argv[2] || __dirname + '/view';
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(publicDir, "/index.html"));
-});
+var server= http.createServer(function(req,res){
 
-app.use(methodOverride());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static(publicDir));
-app.use(errorHandler({
-  dumpExceptions: true,
-  showStack: true
-}));
+    var url = req.url; 
+    //客户端输入的url，例如如果输入localhost:8888/index.html
+    //那么这里的url == /index.html 
 
-console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
-app.listen(port, hostname);
+    var file = publicDir + url;
+    console.log(url);
+
+
+    fs.readFile( file , function(err,data){
+    /*
+        一参为文件路径
+        二参为回调函数
+            回调函数的一参为读取错误返回的信息，返回空就没有错误
+            二参为读取成功返回的文本内容
+    */
+        if(err){
+            res.writeHeader(404,{
+                'content-type' : 'text/html;charset="utf-8"'
+            });
+            res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
+            res.end();
+        }else{
+            res.writeHeader(200,{
+                'content-type' : 'text/html;charset="utf-8"'
+            });
+            res.write(data);//将index.html显示在客户端
+            res.end();
+
+        }
+
+    });
+
+}).listen(port);
+
+console.log("server showing %s listening at http://%s:%s", publicDir, hostname, port);
